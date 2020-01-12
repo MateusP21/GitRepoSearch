488557f247280a73e31f9498b861db348808968c
MateusP21 <mateuspasso20@hotmail.com>
2020-01-12 16:41:46 -0300

    Fix

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
-function git() {
-  var usuario = document.querySelector("input").value
-  var lista = document.querySelector("#lista")
-  axios.get("https://api.github.com/users/" + usuario + "/repos")
+async function git() {
+  
+  const usuario = document.querySelector("input").value
+  const lista = document.getElementById("lista")
+  const usuarioInfo = document.getElementById("user-info")
+  const ul = document.createElement("ul")
+ 
+  const profileImage = document.createElement("img")
+  const userName = document.createElement("h1")
 
-    .then(function (response) {
-      var ul = document.createElement("ul")
+ 
+  const response = await axios.get("https://api.github.com/users/" + usuario + "/repos")
+  .catch(error => {
+    lista.innerHTML = ''
+    const ul = document.createElement("ul")
+    const li = document.createElement("li")
+    const textError = document.createTextNode("Desculpe este usuario não existe :( ")
+    li.appendChild(textError)
+    ul.appendChild(li)
+    lista.appendChild(ul)
+  })
+  const data = response.data
+  data.map((item)=>{
+    usuarioInfo.innerHTML = ""
+    profileImage.src = item.owner.avatar_url
+    userName.textContent = item.owner.login
+    usuarioInfo.appendChild(profileImage)
+    usuarioInfo.appendChild(userName)
+    return usuarioInfo
+  })
 
-      for (repo in response.data) {
+      for(repo in data){
         lista.innerHTML=''
-
-        var textResponse = document.createTextNode(response.data[repo].name)
-        var repoString = JSON.stringify(response.data[repo].name).replace(/"/g,'')
-        var link = document.createElement("a")
+        const textResponse = document.createTextNode(data[repo].name)
+        const repoString = JSON.stringify(data[repo].name).replace(/"/g,'')
+        const link = document.createElement("a")
         link.target="_blank"
         link.href=("https://github.com/" + usuario +"/"+ repoString )
         link.appendChild(textResponse)
-        var li = document.createElement("li")
+        const li = document.createElement("li")
         li.appendChild(link)
         ul.appendChild(li)
         lista.appendChild(ul)
+
       }
-    })
-
-    .catch(function (error) {
-      lista.innerHTML = ''
-      var ul = document.createElement("ul")
-      var li = document.createElement("li")
-      var textError = document.createTextNode("Desculpe este usuario não existe :( ")
-      li.appendChild(textError)
-      ul.appendChild(li)
-      lista.appendChild(ul)
-
-    })
-}
+    }
 
 // CHAMANDO A FUNÇÃO DE BUSCAR
 document.querySelector("body").onkeypress = function(e){
