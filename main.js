bb3da0d6673b3a1a0cbf25f16771c7f83cde27f1
MateusP21 <mateuspasso@outlook.com>
2022-08-12 00:40:08 -0300

    repartindo a função git

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
-const button = document.querySelector("button");
+const button = document.querySelector('button');
+const lista = document.querySelector('ul');
+let usuario = document.querySelector('input');
 
-const lista = document.querySelector("ul");
-async function git() {
-  lista.innerHTML = "";
-  let usuario = document.querySelector("input").value;
-  const usuarioInfo = document.getElementById("user-info");
-  const profileImage = document.createElement("img");
-  const userName = document.createElement("h1");
+function makeRepositoryList(name) {
+  const textResponse = document.createTextNode(name);
+  const link = document.createElement('a');
+  const li = document.createElement('li');
+  link.target = '_blank';
+  link.href = `https://github.com/${usuario.value}/${name}`;
+  link.appendChild(textResponse);
+  li.appendChild(link);
 
-  async function fetchData(user) {
-    if (user === "") return alert("O campo usuario está vazio");
-    const githubData = await (
-      await fetch(`https://api.github.com/users/${user}/repos`)
-    ).json();
-
-    return githubData;
-  }
-
-  const createList = (data) => {
-    if (data.message) {
-      alert(" Desculpe este usuario não existe :( ");
-      document.location.reload();
-    }
-    for (repo of data) {
-      // informações do usuario
-      usuarioInfo.innerHTML = "";
-      profileImage.src = repo.owner.avatar_url;
-      userName.textContent = repo.owner.login;
-      usuarioInfo.appendChild(profileImage);
-      usuarioInfo.appendChild(userName);
-      // criando repositorios
-      const textResponse = document.createTextNode(repo.name);
-      const repoName = repo.name;
-      const link = document.createElement("a");
-      link.target = "_blank";
-      link.href = `https://github.com/${usuario}/${repoName}`;
-      link.appendChild(textResponse);
-      const li = document.createElement("li");
-      li.appendChild(link);
-      lista.appendChild(li);
-    }
-  };
-
-  createList(await fetchData(usuario));
+  return li;
 }
-// CHAMANDO A FUNÇÃO DE BUSCAR
-window.onkeydown = (e) => {
-  e.key === "Enter" && git();
+
+function makeUser(avatar, user) {
+  lista.innerHTML = '';
+  const usuarioInfo = document.getElementById('user-info');
+  const profileImage = document.createElement('img');
+  const userName = document.createElement('h1');
+  usuarioInfo.innerHTML = '';
+  profileImage.src = avatar;
+  userName.textContent = user;
+  usuarioInfo.appendChild(profileImage);
+  usuarioInfo.appendChild(userName);
+}
+
+async function fetchData(user) {
+  if (user === '') return alert('O campo usuario está vazio');
+  const githubData = await (
+    await fetch(`https://api.github.com/users/${user}/repos`)
+  ).json();
+
+  return githubData;
+}
+
+const createList = (data) => {
+  if (data.message) {
+    alert(' Desculpe este usuario não existe :( ');
+    document.location.reload();
+  }
+  makeUser(data[0].owner.avatar_url, data[0].owner.login);
+
+  const repositories = data.map((repo) => {
+    return makeRepositoryList(repo.name);
+  });
+
+  lista.replaceChildren(...repositories);
 };
 
-button.addEventListener("click", () => git());
+async function git() {
+  createList(await fetchData(usuario.value));
+}
+
+// CHAMANDO A FUNÇÃO DE BUSCAR
+window.onkeydown = (e) => {
+  e.key === 'Enter' && git();
+};
+
+button.addEventListener('click', () => git());
